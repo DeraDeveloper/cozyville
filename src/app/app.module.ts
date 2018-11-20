2fec7bd395fe38ee76b25abb02dbb4eaf676b5a5
Chidera Nwoke <cnwoke@seamfix.com>
2018-11-20 18:19:13 +0100

    - Created an app routing modue - Added routing to the app - Added structure to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { SharedService } from './shared-service/shared.service';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AlertModule } from 'ngx-alerts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot({maxMessages: 5, timeout: 5000, position: 'right'})
   ],
-  providers: [],
+  providers: [
+    SharedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  searchBaseURL: 'https://api.unsplash.com/search'
 };
 
 /*
